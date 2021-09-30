59696229ae7a21f6c23dcb545cae596f7121a3f9
James Tutak <tutakjames@gmail.com>
2021-09-30 19:08:19 +0100

    Reverse polish notation without brackets

--- Maths-System/src/sample/calculatorController.java
+++ Maths-System/src/sample/calculatorController.java
@@ -5,24 +5,83 @@ import javafx.fxml.FXML;
 
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+import java.util.HashMap;
+import java.util.Stack;
+
 public class calculatorController {
 
-    String rpn;
+    ArrayList<String> rpn = new ArrayList<>();
     String textInput;
+    String[] charArray;
+    Stack<String> stack = new Stack<String>();
+
+    HashMap<String, Integer> operators = new HashMap<String,Integer>(){{
+        put("=",0);
+        put("+",1);
+        put("-",1);
+        put("*",2);
+        put("/",2);
+        put("^",3);
+        put("(",4);
+    }};
 
     @FXML
     private TextField calcIn;
     @FXML
     private TextField calcOut;
 
-    public String parse(){
+
+    //Parses the input calculation
+    public void parse()  throws EmptyStackException{
 
         textInput = calcIn.getText();
+
+        //Splitting the calculation into single chars
+        charArray = calcIn.getText().split("");
+
+        //Loops through each char in calculation
+        for (String c : charArray){
+            System.out.println(c);
+
+            //If char is operator
+            if (operators.containsKey(c)){
+                System.out.println("IS OPERATOR");
+
+                //Adds char to stack if valid
+                //While loop checks to see if the operator can be added to the stack or if
+                //Stuff needs to be removed from the stack first
+                while (!(stack.empty()) && !(operators.get(c) > operators.get(stack.peek()))) {
+                    System.out.println("CANNOT ADD TO STACK");
+                    rpn.add(stack.pop());
+                }
+
+                //Adds operator to stack
+                stack.push(c);
+            }
+            //If the operand is a ')' Then the stack will be popped until the '(' is found.
+            else if (c.equals(")")){
+                while (!stack.empty() && !stack.peek().equals("(")){
+                    rpn.add(stack.pop());
+                }
+            } else {
+                rpn.add(c);
+            }
+        }
+
+        while(!stack.empty()){
+            rpn.add(stack.pop());
+        }
+
+        System.out.println("Printing rpn");
+        System.out.println(rpn);
+
+
 
         calcOut.setText(textInput);
 
 
-        return rpn;
     }
 
     
