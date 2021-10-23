443475a2cf037ddf02a6e055a36338a85e8db4f4
Premil-Mathews <57670067+Premil-Mathews@users.noreply.github.com>
2021-10-23 16:34:03 +0100

    Lexer and parser working

--- Maths-System/src/sample/Lexer.java
+++ Maths-System/src/sample/Lexer.java
@@ -3,18 +3,21 @@ import java.util.Scanner;
 
 public class Lexer {
 
-    private static int T_LPAR = 1;         // (
-    private static int T_RPAR = 2;         // )
-    private static int T_DIV = 3;          // %
-    private static int T_MULTIPLY = 4;     // *
-    private static int T_ADD = 5;          // +
-    private static int T_SUBTRACT = 6;     // -
-    private static int T_IDENTIFIER = 7;   // [A-Z] | [a-z]
-    private static int T_NUMBER = 8;       // [0-9]
-    private static int T_EQUAL = 9;        //=
-    private static int T_DECIMAL = 10;     // .
+    public static int T_LPAR = 1;         // (
+    public static int T_RPAR = 2;         // )
+    public static int T_DIV = 3;          // %
+    public static int T_MULTIPLY = 4;     // *
+    public static int T_ADD = 5;          // +
+    public static int T_SUBTRACT = 6;     // -
+    public static int T_IDENTIFIER = 7;   // [A-Z] | [a-z]
+    public static int T_NUMBER = 8;       // [0-9]
+    public static int T_EQUAL = 9;        //=
+    public static int T_DECIMAL = 10;     // .
 
     private static int MAX=64;
+    public int NR_tokens;
+    public int[] Tokens;
+    public int[] SymbolTable;
 
     public int[] lexer(String input){
         char lexeme[] = new char[MAX];
@@ -22,6 +25,8 @@ public class Lexer {
         input = input.replaceAll("\\s+","");
         lexeme = input.toCharArray();
         int values[] = new int[MAX];
+        String num = new String();
+        int counter=0;
 //        test lexeme array
 //        for(char a:lexeme){
 //            System.out.print(a);
@@ -33,41 +38,67 @@ public class Lexer {
             switch (a){
                 case ' ':
                     break;
-                case '(': tokens[i] = T_LPAR;
-                    values[i]=-1;
+                case '(': tokens[counter] = T_LPAR;
+                    values[counter]=-1;
+                    counter++;
                     break;
-                case ')': tokens[i] = T_RPAR;
-                    values[i]=-1;
+                case ')': tokens[counter] = T_RPAR;
+                    values[counter]=-1;
+                    counter++;
                     break;
-                case '/': tokens[i] = T_DIV;
-                    values[i]=-1;
+                case '/': tokens[counter] = T_DIV;
+                    values[counter]=-1;
+                    counter++;
                     break;
-                case '*': tokens[i] = T_MULTIPLY;
-                    values[i]=-1;
+                case '*': tokens[counter] = T_MULTIPLY;
+                    values[counter]=-1;
+                    counter++;
                     break;
-                case '+': tokens[i] = T_ADD;
-                    values[i]=-1;
+                case '+': tokens[counter] = T_ADD;
+                    values[counter]=-1;
+                    counter++;
                     break;
-                case '-': tokens[i] = T_SUBTRACT;
-                    values[i]=-1;
+                case '-': tokens[counter] = T_SUBTRACT;
+                    values[counter]=-1;
+                    counter++;
                     break;
-                case '.': tokens[i] = T_DECIMAL;
-                    values[i]=-1;
+                case '.': tokens[counter] = T_DECIMAL;
+                    values[counter]=-1;
+                    counter++;
                     break;
-                case '=': tokens[i] = T_EQUAL;
-                    values[i]=-1;
+                case '=': tokens[counter] = T_EQUAL;
+                    values[counter]=-1;
+                    counter++;
                     break;
                 default:
-                    Boolean flag = Character.isDigit(a);
-                    if (flag){
-                        tokens[i] = T_NUMBER;
-                        values[i] = Character.getNumericValue(a);
+                    if (Character.isDigit(a)){
+                        tokens[counter] = T_NUMBER;
+                        num+=a;
+//                        System.out.print(a+"\n");
+                        while (Character.isDigit(lexeme[i++]) && i<lexeme.length){
+                            if (Character.isDigit(lexeme[i])){
+                                num+=lexeme[i];
+//                                    System.out.print(num + "\n");
+                            }
+                            else {
+//                                    System.out.print(num + "\n");
+//                                    System.out.print(i + "\n");
+                                break;
+
+                            }
+                        }
+                        values[counter] = Integer.parseInt(num);
+//                        System.out.print(values[counter] + "\n");
+                        num="";
+                        i--;
+                        counter++;
                         break;
                     }
                     Boolean flag2 = Character.isLetter(a);
                     if (flag2){
-                        tokens[i] = T_IDENTIFIER;
-                        values[i]=a;
+                        tokens[counter] = T_IDENTIFIER;
+                        values[counter]=a;
+                        counter++;
                         break;
                     }
                     else{
@@ -76,15 +107,18 @@ public class Lexer {
                     }
                     break;
             }
+            NR_tokens = counter;
+            Tokens = tokens;
+            SymbolTable = values;
         }
 //        test tokens[] list output
         for (int i=0; i< tokens.length; i++){
             if(tokens[i]!=0) {
                 System.out.print("Token: "+i +", TokenID: "+tokens[i]+", value:"+values[i]);
                 System.out.print('\n');
+//                System.out.print(tokens[i]+"\n");
             }
         }
-
 
         return tokens;
     }
@@ -94,7 +128,17 @@ public class Lexer {
         Scanner obj = new Scanner(System.in);
         System.out.print("Enter expression: \n");
         String expression = obj.nextLine();
-        Lexer one = new Lexer();
+//        Lexer one = new Lexer();
+//        one.lexer(expression);
+        Parse one = new Parse();
         one.lexer(expression);
+        System.out.print("\n");
+        if(one.parse() == 1){
+            System.out.print("PARSING SUCCESSFUL");
+        }
+        else {
+            System.out.print("PARSING FAILED");
+        }
+
     }
 }
---
+++ Maths-System/src/sample/Parser.java
@@ -0,0 +1,115 @@
+package sample;
+// BNF rules
+// <expr>	::=	<term> <expr'>
+// <expr'>	::= <operator> <term> <expr'> | <empty>
+// <term>	::= <factor> <term'>
+// <term'>	::= <operator> <factor> <term'> | <empty>
+// <factor> ::= number | (<expr>)
+// <operator> ::= <division> | <multiplication> | <add> | <subtract>
+// <division> ::=  ÷
+// <multiplication> ::= *
+// <add> ::= +
+// <subtract> ::= -
+// <number> ::= 0|1|2|3|4|5|6|7|8|9
+
+
+class Parse extends Lexer{
+
+    int lookahead;
+    int currentToken;
+    int ret;
+    int level;
+
+
+    boolean match(int token){
+        if (lookahead==-1){
+            lookahead = Tokens[currentToken];
+        }
+        if (token==lookahead){
+            //System.out.print("Token match"+"\n");
+            return true;
+        }
+        else{
+            //System.out.print("Tokens do not match on level: " + level+"\n");
+            return false;
+        }
+    }
+
+    void advance(int level){
+        lookahead = Tokens[currentToken++];
+        System.out.print("Advance() called at level: "+level+"\n");
+    }
+
+    public int parse(){
+        System.out.print("PARSER STARTING"+"\n");
+//        for (int i=0; i<NR_tokens; i++){
+//            System.out.print(i+"\n");
+//        }
+        currentToken = 0;
+        lookahead = -1;
+        ret = 1;
+        expression(0);
+        if (currentToken<NR_tokens){
+            System.out.print("SYNTAX ERROR- Token: "+currentToken+" of value: "+SymbolTable[currentToken] + "\n");
+            ret = 0;
+        }
+        return ret;
+    }
+
+    void expression(int level){
+        System.out.print("Expression() called at level: "+level+"\n");
+        term(level+1);
+        expression_p(level+1);
+    }
+    void term(int level){
+        System.out.print("term() called at level: "+level+"\n");
+        factor(level+1);
+        term_p(level+1);
+    }
+    void term_p(int level){
+        System.out.print("term_p() called at level: "+level+"\n");
+        if (match(T_MULTIPLY) || match(T_DIV) || match(T_ADD) || match(T_SUBTRACT)){
+            advance(level+1);
+            factor(level+1);
+            term_p(level+1);
+        }
+    }
+
+    void expression_p(int level){
+        System.out.print("expression_p() called at level: "+level+"\n");
+        if (match(T_MULTIPLY) || match(T_DIV) || match(T_ADD) || match(T_SUBTRACT)){
+            advance(level+1);
+            term(level+1);
+            expression_p(level+1);
+        }
+    }
+
+    void factor(int level){
+        System.out.print("factor() called at level: "+level+"\n");
+        if (match(T_LPAR)){
+            advance(level+1);
+            expression(level+1);
+            if (match(T_RPAR)){
+                advance(level+1);
+
+            }
+            else {
+                System.out.print("Syntax error: Mismatched parentheses \n");
+                ret = 0;
+            }
+        }
+        if (match(T_NUMBER)){
+            advance(level+1);
+            term(level+1);
+            expression_p(level+1);
+        }
+        else if (match(T_IDENTIFIER)){
+            advance(level+1);
+            term(level+1);
+            expression_p(level+1);
+        }
+
+    }
+
+
+}
